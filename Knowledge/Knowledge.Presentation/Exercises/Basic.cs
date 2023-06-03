2470084b2462b104079f7ab39895cdd1168ad36a
samuhuse <samuele.lombardi@nexusat.it>
2023-06-03 13:07:25 +0200

    added Basic ex class

---
+++ Knowledge/Knowledge.Presentation/Exercises/Basic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge.Presentation.Exercises
+{
+     public static class Basic
+    {
+        public static void Es1()
+        {
+            Console.WriteLine("hello");
+            Console.WriteLine("Alessandro");
+        }
+
+        public static void Es2()
+        {
+
+        }
+
+    }
+}
